91330b6e4eb6ccfc80f65844a310b182ff499c48
祐群 <fox-lalala@hotmail.com>
2019-11-22 10:01:42 +0800

    protractor 設定檔認識

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,13 +10,13 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 600000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './src/**/*.e2e-spec.ts' // 要進行測試的檔案
   ],
   capabilities: {
-    browserName: 'chrome',
+    browserName: 'chrome', // 要執行的瀏覽器
     chromeOptions: {
       args: [
-        // "--headless",
+        // "--headless", // 做 headless , 如果想看到頁面記得打開 --window-size
         // "--window-size=1920,1080",
         // "--incognito",
         // "--start-maximized"
@@ -26,26 +26,29 @@ exports.config = {
 
   // SELENIUM_PROMISE_MANAGER: false,
 
-  directConnect: true,
+  directConnect: true, // 設定直接控制瀏覽器
   //seleniumServerJar: '../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar',
   baseUrl: 'http://localhost:4200/',
 
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 100000000,
-    print: function() {}
+    defaultTimeoutInterval: 100000000, // 每一個 it 執行的 timeout 時間
+    print: function () { }
   },
 
+
+  /** 執行測試前的準備工作 */
   async onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
 
+    // 設定報告的方式
     jasmine.getEnv().addReporter(new SpecReporter({
       // https://github.com/bcaudan/jasmine-spec-reporter/blob/master/src/configuration.ts
       spec: {
-        displayStacktrace: false
+        displayStacktrace: false // 關閉完整的錯誤訊息
       }
     }));
 
