bb6f5c98425cbecdda5c9ab478395fc31ff21c94
祐群 <fox-lalala@hotmail.com>
2019-11-22 15:45:10 +0800

    實戰演練 4 - 體驗卡關感覺 目標站點有 setInterval , 造成 ngZone 永遠不穩定

--- src/labs/questionnaire.e2e-spec.ts
+++ src/labs/questionnaire.e2e-spec.ts
@@ -1,4 +1,20 @@
+import { browser, by, element } from 'protractor';
 
-describe('the user submit a questionnaire', () => {
+describe('體驗卡關的感覺', () => {
+    it('測試- ngZone', async () => {
+        // 跳脫 ngZone 的方法之一 : 沒這行就會卡住
+        await browser.waitForAngularEnabled(false);
+
+        await browser.get('/labs/questionnaire');
+
+        await element(by.name('username')).sendKeys('John');
+        await element(by.name('codeLanguage')).sendKeys('C#');
+        await element(by.buttonText('送出')).click();
+
+        const result = await element(by.css('form')).getText();
+        expect(result).toContain('送出成功');
+
+        await browser.waitForAngularEnabled(true);
+    });
 
 });
